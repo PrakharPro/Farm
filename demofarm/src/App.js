db8b88c913bf583c44aac321d7812555a2972439
Prakhar Rajput <prakharrajput@Prakhars-MacBook-Air.local>
2023-05-19 11:36:50 +0530

    made changes for flexbox

--- demofarm/src/App.js
+++ demofarm/src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
+import Char1 from './Images/Char1.jpeg';
+import Char2 from './Images/Char2.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div id="Box1">
+        <div id="chair1">
+            <img src={Char1} /> 
+            <div id="TextChair1" ><p>Lounge Chair</p>
+            <strong>$149</strong>
+            </div>
+        </div>
+      </div>
+
+     <div id="Box3">
+      <p>Up To 20% off</p>
+      <span>
+        SPRING OFFER
+      </span>
+      <p >Shop now</p>
+     </div>
+
+      <div id="Box2">
+      <img src={Char2} />
+      <div id="TextChair2" ><p>Take it outside Carpet</p>
+            <strong>$500</strong>
+            </div>
+      </div>
     </div>
   );
 }
